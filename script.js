9be484e9d15078e46d66428d6b34df05884770c6
cv-net <khalil.mktg@gmail.com>
2021-01-13 08:43:12 -0500

    started JS

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // if someone clicked before you, switch colors?
-$('.box').click(function () {
-    
-});
+// if (player = 0) {} else {}
+
 
 // if someone clicked this before, you can't click it
+let currentPlayer = 0;
+
+let allBoxes = document.getElementsByClassName('box');
+
+//click, if player 0, change color, say its player1s turn, change currentplayer
+allBoxes.addEventListener('click', () => {
+    if (currentPlayer == 0) {
+        target.style.cssText = "color: coral";
+    } else {
+        target.style.cssText = "color: blue";
+    }
+});
+
+
 
 
